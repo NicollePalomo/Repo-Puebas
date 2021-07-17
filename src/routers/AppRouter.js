386e5df53b86c9c4f98a386cffb29d00fa40777b
Nicolle Palomo R <nicollepalomo68@gmail.com>
2021-07-17 14:57:59 -0500

    despliegue de la pagina de prueba

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+export default function AppRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <h1>About</h1>
+        </Route>
+
+        <Route path="/contact">
+          <h1>Contact</h1>
+        </Route>
+
+        <Route exact path="/">
+          <h1>Home hola que onda </h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
